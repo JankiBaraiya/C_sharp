5bb146ba22eccef76b23c6f38a912c3e222205a1
JankiBaraiya <108472423+JankiBaraiya@users.noreply.github.com>
2023-07-19 19:13:35 +0530

    Create L1p1.cs

---
+++ Lab_1/L1p1.cs
@@ -0,0 +1,21 @@
+/*Write a program to print your name, address, contact number & city.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class L1P1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Name: Janki");
+            Console.WriteLine("Address: GokulPark2 - Dhrol");
+            Console.WriteLine("Contact: 1234562236");
+            Console.WriteLine("City: Dhrol");
+        }
+    }
+}
