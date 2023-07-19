3711aa973b76ff6ed89e036348ccca3d0d77bd09
JankiBaraiya <108472423+JankiBaraiya@users.noreply.github.com>
2023-07-19 19:09:51 +0530

    Add files via upload

---
+++ Lab_4/L4P10.cs
@@ -0,0 +1,25 @@
+﻿/* Write a program to change the case of entered character.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class L4P10
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a character : ");
+            char c = Console.ReadLine().ToCharArray()[0];
+            char ans;
+            if (Char.IsUpper(c))
+                ans = Char.ToLower(c);
+            else
+                ans = Char.ToUpper(c);
+            Console.WriteLine("Changed case of character : " + ans);
+        }
+    }
+}
---
+++ Lab_4/L4P9.cs
@@ -0,0 +1,32 @@
+﻿/* Write a program to find the longest word in a string.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class L4P9
+    {
+        public static void Main()
+        {
+            string line = "Write a program to find the longest word in a string.";
+            Console.WriteLine(line);
+            string[] words = line.Split(new[] { " " }, StringSplitOptions.None);
+            string word = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    word = s;
+                    ctr = s.Length;
+                }
+            }
+            Console.WriteLine("Longest word: {0}", word);
+        }
+
+    }
+}
